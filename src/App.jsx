1fbda25c2d588e2dcfcbe6112c37794d10321f72
Maelle AMADOU <amadoumaelle@gmail.com>
2026-04-27 13:29:19 +0200

    chore(setup): Initialiser le projet avec React.js et Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div> 
+      <h1> Bienvenue sur cette Application </h1>
+    </div>
+  )
+}
+
+export default App
